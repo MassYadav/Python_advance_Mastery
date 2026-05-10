d8a9ba07f021db5e81e3a90e839c65b74e838a66
Manish <mky80094@gmail.com>
2026-05-10 10:23:36 +0530

    _______

---
+++ basic.py
@@ -0,0 +1,19 @@
+
+# functions
+def hello():
+    print("Hello, World!")
+
+hello()
+
+def name_age(name, age):
+    print(f"Name: {name}, Age: {age}")
+
+name_age(age=30, name="sdfdsa")
+
+def sum(a, b):
+    print(f"The sum of {a} and {b} is : {a+b}")
+
+
+sum(5,10)
+sum(3.5,4.5)
+
