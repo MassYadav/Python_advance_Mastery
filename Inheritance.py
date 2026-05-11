e5c312084a07e6652d79e05e1c32530d1d96231e
Manish <mky80094@gmail.com>
2026-05-11 12:06:57 +0530

    mul inheritance and mutiple also

--- Inheritance.py
+++ Inheritance.py
@@ -12,19 +12,37 @@
 # obj2.hello()
 
 
-class Animal:
-    def __init__(self,name):
-        self.name = name
-    def show(self):
-        print(f"the name is {self.name},{self.age}")
+# class Animal:
+#     def __init__(self,name):
+#         self.name = name
+#     def show(self):
+#         print(f"the name is {self.name},{self.age}")
 
-class Dog(Animal):
-    def __init__(self,name,age):
-        super().__init__(name)
-        self.age = age
-    def show(self):
-        print(f"the name is {self.name}, {self.age} years old")    
-obj1 = Animal("timm")
+# class Dog(Animal):
+#     def __init__(self,name,age):
+#         super().__init__(name)
+#         self.age = age
+#     def show(self):
+#         print(f"the name is {self.name}, {self.age} years old")    
+# obj1 = Animal("timm")
 
-obj = Dog("timm",5)
-obj.show()
+# obj = Dog("timm",5)
+# obj.show()
+class Factory:
+    def __init__(self,material,zips):
+        self.material = material
+        self.zips = zips
+
+class PuneFactory:
+    def __init__(self,material,zips,pockets):
+        super().__init__(material,zips)
+        self.pockets = pockets
+class MumbaiFactory:
+    def __init__(self,material,zips,pockets,colour):
+        super().__init__(material,zips,pockets)
+        self.colour = colour        
+
+obj = PuneFactory("leather",2,4)
+print(obj.material)
+obj2 = MumbaiFactory("canvas",1,2,"red")
+print(obj2.colour)
